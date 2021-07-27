bf0ef4965e4000a45fb01e7f1ddd84786572a6a8
Egor <egor.haritonov5618@gmail.com>
2021-07-27 23:22:55 +0400

    Добавил созданное приложение в ссылки

--- kvartirka_blog/settings.py
+++ kvartirka_blog/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog_kvartirka',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
