c809c2942672ab42de7069939c4a56c4fd5857ca
Egor <egor.haritonov5618@gmail.com>
2021-07-30 19:18:50 +0400

    Добавил классы для взаимодействия с моделями Post и Comment

--- blog_kvartirka/admin.py
+++ blog_kvartirka/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'body', 'comments')
+    list_filter = ('date_publication', 'slug')
+    search_fields = ('title', 'body', 'data_publication', 'slug')
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'content_type', 'created', 'content_object')
+    list_filter = ('created', 'updated')
+    search_fields = ('name', 'object_id', 'body')
+    autocomplete_lookup_fields = {
+        'generic': [['content_type', 'object_id']],
+    }
+
+
+admin.site.register(Comment, CommentAdmin)
